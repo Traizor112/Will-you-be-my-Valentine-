f73e90acde9434d802a15d63576b3c5528ca53ad
Traizor112 <131078928+Traizor112@users.noreply.github.com>
2025-02-11 17:50:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Emin misin?",
+    "Gerçekten emin misin??",
+    "Olumlu musun?",
+    "Pookie lütfen...",
+    "Bunu düşün.",
+    "Asla dersen çok üzülürüm...",
+    "çok üzüleceğim...",
+    "çok çok çok üzüleceğim...",
+    "Tamam tamam sormayı bırakacağım...",
+    "Şaka yapıyorum, evet deyin lütfen!"
 ];
 
 let messageIndex = 0;
